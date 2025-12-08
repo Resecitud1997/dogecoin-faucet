c7ba6791bd16a79348eb52bf6fff45ecf2257af6
Resecitud1997 <resecitud@gmail.com>
2025-12-07 21:03:26 -0300

    Update dogecoin.js

--- backend/src/services/dogecoin.js
+++ backend/src/services/dogecoin.js
@@ -1,1 +1,17 @@
+// backend/src/services/dogecoin.js
+const bitcoin = require('bitcoin');
+const client = new bitcoin.Client({
+  host: process.env.DOGE_RPC_HOST,
+  port: process.env.DOGE_RPC_PORT,
+  user: process.env.DOGE_RPC_USER,
+  pass: process.env.DOGE_RPC_PASSWORD
+});
 
+async function sendDogecoin(address, amount) {
+  return new Promise((resolve, reject) => {
+    client.sendToAddress(address, amount, (err, txid) => {
+      if (err) reject(err);
+      else resolve(txid);
+    });
+  });
+}
